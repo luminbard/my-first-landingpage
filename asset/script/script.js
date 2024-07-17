f013af408128048eb3cb788ed64b47b92e909023
lumin <luminbard@gmail.com>
2024-07-17 21:41:40 +0900

    fea:finished I will test

--- asset/script/script.js
+++ asset/script/script.js
@@ -4,26 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const rightNav = document.getElementById("right-nav");
     const logo = document.getElementById("logo");
     const signUp = document.getElementById("sign-up");
+    const topLink = document.getElementById("top-link");
     const firstNav = document.getElementById("first-nav");
     hamburger.addEventListener("click", () => {
         navbarLinks.classList.toggle("active");
-        rightNav.classList.toggle("active");
+        rightNav.classList.toggle("active"); 
         signUp.classList.toggle("active");
         logo.classList.toggle("hide");
+        topLink.classList.toggle("hide");
         if (firstNav.style.justifyContent=="end")
             firstNav.style.justifyContent="space-between"
         else
             firstNav.style.justifyContent="end";
-            //setBodyHeight(658);
         hamburger.classList.toggle("change");
-        
     });
 });
-
-
-function setBodyHeight(height) {
-    document.body.style.height = height + 'px';
-}
 
 
 document.querySelectorAll('.faq-item').forEach(item => {
