97f521dea5debac84e26e619fbae2bf0ece6bdfe
lumin <luminbard@gmail.com>
2024-07-21 16:35:01 +0900

    fea: PC refinished

--- asset/script/script.js
+++ asset/script/script.js
@@ -41,12 +41,7 @@ document.querySelectorAll('.faq-item').forEach(item => {
 
 function setIframeSrc() {
     var iframe = document.getElementById('image-map');
-    if (window.innerWidth < 1100) {
-        iframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5452.16234944011!2d139.3357881570759!3d35.655448057382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60191dd856de197f%3A0xe206cd84f9aec58d!2z44Kz44Ov44O844Kt44Oz44Kw44K544Oa44O844K55YWr546L5a2QIDhCZWF0!5e0!3m2!1sja!2sjp!4v1721228943167!5m2!1sja!2sjp" ;;
-    } else {
-        iframe.src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6483.5679417952115!2d139.33263113371498!3d35.657693758640264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1721228275270!5m2!1sja!2sjp";
-                               
-    }
+    iframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.790021683318!2d139.33721069999999!3d35.657544699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60191d36c0322575%3A0xa366f8ae3c4953e0!2sDESIGN%20CAMPUS!5e0!3m2!1sja!2s!4v1721545720272!5m2!1sja!2s";
 }
 
 // Set the src when the page loads
